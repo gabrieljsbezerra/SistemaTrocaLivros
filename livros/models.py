c6391900be5dfd1ce98bbd29e755c055e66b0d85
Gabriel Joao <gabriel_jb30@hotmail.com>
2024-10-29 23:10:15 -0300

    Inicial_push

---
+++ livros/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from datetime import date
+
+class TabelaLivros(models.Model):
+    nome_livro = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    co_autor = models.CharField(max_length=50, blank = True, null=True)
+    editora = models.CharField(max_length=30)
+    cateoria = models.CharField(max_length=30)
+    data_cadastro = models.DateField(default=date.today)

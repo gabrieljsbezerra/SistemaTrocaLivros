0e8406af8a3276ef6724968856b211700795c6f6
Gabriel Joao <gabriel_jb30@hotmail.com>
2024-11-02 18:24:39 -0300

    - Criada uma tabela de Categoria, para armazenar as categorias dos Livros separadamente - Criado campo de chave estrangeira para as categorias da tabela livros - makemigrations e migrate executados com sucesso

--- livros/models.py
+++ livros/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from datetime import date
 
+class TabelaCategorias(models.Model):
+    nome_categoria = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=60)
+
+    class Meta:
+        verbose_name = 'Tabela_Categoria'
+
+    def __str__(self):
+        return self.nome_categoria
+
+
 
 class TabelaLivros(models.Model):
     nome_livro = models.CharField(max_length=100)
     autor = models.CharField(max_length=50)
     co_autor = models.CharField(max_length=50, blank=True, null=True)
     editora = models.CharField(max_length=30)
-    cateoria = models.CharField(max_length=30)
+    categoria = models.ForeignKey(TabelaCategorias, on_delete=models.DO_NOTHING)
     braile = models.BooleanField(default=False)
     data_cadastro = models.DateField(default=date.today)
 

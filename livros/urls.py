64a967eddf387a5a85436e0e10668deb5ee02590
Gabriel Joao <gabriel_jb30@hotmail.com>
2024-11-02 22:31:13 -0300

    - Página de meus livros funcionando adequadamente - Pesquisa de livros implementada - Home page criada

--- livros/urls.py
+++ livros/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('Meus Livros/', views.meus_livros, name='meus_livros'),
 ]
--- livros/views.py
+++ livros/views.py
@@ -2,12 +2,32 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.urls import reverse
 from usuarios.models import TabelaUsuarios
+from .models import TabelaLivros
+from django.db.models import Q
 
 
 def home(request):
     if request.session.get('usuarios'):
-        usuario = TabelaUsuarios.objects.get(
-            id=request.session['usuarios']).nome_usuario
-        return render(request, 'home.html')
+        usuario = TabelaUsuarios.objects.get(id=request.session['usuarios']).nome_usuario
+        livros = TabelaLivros.objects.all()
+        return render(request, 'home.html', {'livros':livros})
+        
     else:
         return redirect(f"{reverse('login')}?status=2")
+    
+def meus_livros(request):
+    if request.session.get('usuarios'):
+        usuario = TabelaUsuarios.objects.get(id=request.session['usuarios']).nome_usuario
+        query = request.GET.get('q', '')  # Obtém o termo de pesquisa
+
+        # Filtra os livros com base no termo de pesquisa
+        livros = TabelaLivros.objects.filter(
+            Q(nome_livro__icontains=query) | 
+            Q(autor__icontains=query) | 
+            Q(editora__icontains=query)
+        ) if query else TabelaLivros.objects.all()
+
+        return render(request, 'meus_livros.html', {'livros': livros, 'query': query})
+    else:
+        return redirect(f"{reverse('login')}?status=2")
+        

5e702c4d8732d07399c11d3c1a2171aed62fbfa7
Gabriel Joao <gabriel_jb30@hotmail.com>
2024-11-08 02:30:40 -0300

    - Adicionada Tabela Trocas no ambiente administrativo do Django

--- livros/admin.py
+++ livros/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from . models import TabelaLivros, TabelaCategorias
+from . models import TabelaLivros, TabelaCategorias, TabelaTrocas
 
 #Registrando minhas tabelas na parte administrativa
 admin.site.register(TabelaLivros)
 
 admin.site.register(TabelaCategorias)
+
+admin.site.register(TabelaTrocas)
